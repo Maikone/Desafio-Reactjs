050b2c92cb067de86f0547764f3cd425c7f341a5
maiKone <michaelmrmj@gmail.com>
2020-11-10 00:03:30 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from "react";
+
+
+import "./styles.css";
+import api from "./services/api";
+
+function App() {
+const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  },[]);
+
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories',{
+      title: 'Ferguile',
+      url:  'Ferguile.com',
+      techs: ['Copias', 'Sofas']
+    })
+
+    setRepositories([ ...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+    
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
+
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>
+          ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
